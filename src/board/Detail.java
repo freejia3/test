dea146b6219074ecac6791e9113cac16c4d75f33
freejia3 <37688043+freejia3@users.noreply.github.com>
2018-05-13 01:03:04 +0900

    Update Detail.java

--- src/board/Detail.java
+++ src/board/Detail.java
@@ -9,7 +9,7 @@ import model.DAO;
 
 public class Detail implements Action {
 
-	//hihi
+	//ryujia
 	@Override
 	public ActionData execute(HttpServletRequest request, HttpServletResponse response) {
 		int id = Integer.parseInt(request.getParameter("id"));
